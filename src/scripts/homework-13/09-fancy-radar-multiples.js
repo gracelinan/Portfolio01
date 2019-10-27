ba37e748c6f7e1a0497615c3b47edfeddf2185f2
Grace <grace@Nans-MacBook-Air.local>
2019-10-27 02:53:51 -0400

    new project

--- src/scripts/homework-13/09-fancy-radar-multiples.js
+++ src/scripts/homework-13/09-fancy-radar-multiples.js
@@ -1,14 +1,144 @@
-import * as d3 from 'd3'
+// import * as d3 from 'd3'
 
-const margin = { top: 20, left: 0, right: 0, bottom: 0 }
+// const margin = { top: 20, left: 0, right: 0, bottom: 0 }
 
-const height = 330 - margin.top - margin.bottom
-const width = 275 - margin.left - margin.right
+// const height = 330 - margin.top - margin.bottom
+// const width = 275 - margin.left - margin.right
 
-d3.csv(require('/data/homework-13/nba.csv'))
-  .then(ready)
-  .catch(err => console.log('Failed with', err))
+// const container = d3.select('#chart-9')
 
-function ready(datapoints) {
-  
-}
+// const angleScale = d3.scaleBand().range([0, Math.PI * 2])
+
+// const radius = 100
+
+// const radiusScale = d3
+//   .scaleLinear()
+//   .domain([0, 10])
+//   .range([0, radius])
+
+// const line = d3
+//   .radialArea()
+//   .angle(d => angleScale(d.name))
+//   .innerRadius(0)
+//   .outerRadius(d => radiusScale(d.value))
+
+// d3.csv(require('/data/homework-13/nba.csv'))
+//   .then(ready)
+//   .catch(err => console.log('Failed with', err))
+
+// function ready(datapoints) {
+
+//   const maxMinutes = 60
+//   const maxPoints = 30
+//   const max3Point = 5
+//   const maxRebounds = 15
+//   const maxSteals = 5
+//   const maxBlocks = 5
+
+//   const customDatapoints = [
+//     { name: 'Minutes', value: (datapoints.MP / maxMinutes) * 10 },
+//     { name: 'Points', value: (datapoints.PTS / maxPoints) * 10 },
+//     { name: 'Field Goals', value: datapoints.FG },
+//     { name: '3-Point Field Goals', value: (datapoints['3P'] / max3Point) * 10 },
+//     { name: 'Free Throws', value: datapoints.FT },
+//     { name: 'Rebounds', value: (datapoints.TRB / maxRebounds) * 10 },
+//     { name: 'Assists', value: datapoints.AST },
+//     { name: 'Steals', value: (datapoints.STL / maxSteals) * 10 },
+//     { name: 'Blocks', value: (datapoints.BLK / maxBlocks) * 10 }
+//   ]
+
+//   const categories = customDatapoints.map(d => d.name)
+//   angleScale.domain(categories)
+
+//   container
+//      .selectAll('svg')
+//     .data(datapoints)
+//     .enter()
+//    .append('svg')
+//      .attr('height', height + margin.top + margin.bottom)
+//      .attr('width', width + margin.left + margin.right)
+//   .append('g')
+//    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+//      .append('g')
+//    .attr('transform', `translate(${width / 2},${height / 2})`)
+//    .each(function(d) {
+//       console.log(datapoints)
+//        const svg = d3.select(this)
+      
+//   const bands = [2, 4, 6, 8, 10]
+//   // Draw a circle for each item in bands
+//   svg
+//     .selectAll('.band')
+//     .data(bands)
+//     .enter()
+//     .append('circle')
+//     .attr('fill', 'none')
+//     .attr('stroke', 'lightgrey')
+//     .attr('r', function(d) {
+//       return radiusScale(d)
+//     })
+//     .attr('fill', function(d, i) {
+//       console.log('Looking at circle number', i % 2)
+//       if (i % 2 === 0) {
+//         return '#c94435'
+//       } else {
+//         return '#FFB81C'
+//       }
+//     })
+//     //.attr('mask', 'url(#playerMask)')
+//     .lower()
+    
+//     // svg
+//     // .selectAll('.band')
+//     // .data(bands)
+//     // .enter()
+//     // .append('circle')
+//     // .attr('fill', 'none')
+//     // .attr('stroke', 'lightgrey')
+//     // .attr('r', function(d) {
+//     //   return radiusScale(d)
+//     // })
+//     // .attr('fill', function(d, i) {
+//     //   console.log('Looking at circle number', i % 2)
+//     //   if (i % 2 === 0) {
+//     //     return '#e8e7e5'
+//     //   } else {
+//     //     return '#f6f6f6'
+//     //   }
+//     // })
+//     // .lower()
+
+//     svg.
+//     append('text').text('0')
+//     .attr('text-anchor', 'middle')
+//     .attr('alignment-baseline', 'middle')
+
+//     svg
+//     .selectAll('.outside-label')
+//     .data(angleScale.domain())
+//     .enter()
+//     .append('text')
+//     .attr('font-weight', '700')
+//     .text(d => d)
+//     .attr('y', -radius) // set it up at the top of the chart
+//     .attr('dy', -15) // give a little offset to push it higher
+//     .attr('transform', function(d) {
+//       return `rotate(${(angleScale(d) * 180) / Math.PI})`
+//     })
+//     .attr('text-anchor', 'middle')
+//     .attr('alignment-baseline', 'middle')
+//     .style('font-size', 10)
+
+//     svg
+//     .append('mask')
+//     .datum(customDatapoints, function (d, i) {
+//       console.log(customDatapoints[i])
+//       return customDatapoints[i]})
+//     .attr('id', d.Name.replace(' ', '-'))
+//     .append('path')
+//     .attr('d', line)
+//     .attr('fill', 'white')
+//     .attr('stroke', 'white')
+
+//       })
+// }
---
+++ src/scripts/project-03/chart-01.js
@@ -0,0 +1,126 @@
+import * as d3 from 'd3'
+import d3Tip from 'd3-tip'
+import d3Annotation from 'd3-svg-annotation'
+
+d3.tip = d3Tip
+
+const margin = {
+  top: 30,
+  right: 20,
+  bottom: 30,
+  left: 20
+}
+
+const width = 700 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
+
+const svg = d3
+  .select('#bar-chart')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+const xPositionScale = d3.scaleBand().range([0, width])
+
+const yPositionScale = d3
+  .scaleLinear()
+  .range([height, 0])
+
+const tip = d3
+  .tip()
+  .attr('class', 'd3-tip')
+  .offset([0, 0])
+  .html(function(d) {
+    return `${d.Country_Name} <span style='color:red'>${d.Cost_1_GB_USD}</span>`
+  })
+
+svg.call(tip)
+
+d3.csv(require('/data/project-03/Mobile-Pricing-2019Q2.csv')).then(ready)
+
+function ready(datapoints) {
+  // Sort the countries from low to high
+  datapoints = datapoints.sort((a, b) => {
+    return a.Cost_1_GB_USD - b.Cost_1_GB_USD
+  })
+
+  // And set up the domain of the xPositionScale
+  // using the read-in data
+  const countries = datapoints.map(d => d.Country_Name)
+  xPositionScale.domain(countries)
+
+  const costs = datapoints.map(d => +d.Cost_1_GB_USD)
+  const costMax = d3.max(costs)
+  console.log(costMax)
+  yPositionScale.domain([0, 15])
+
+  /* Add your rectangles here */
+  svg
+    .selectAll('rect')
+    .data(datapoints)
+    .enter()
+    .append('rect')
+    .attr('width', xPositionScale.bandwidth())
+    .attr('height', d => height - yPositionScale(d.Cost_1_GB_USD))
+    .attr('x', d => xPositionScale(d.Country_Name))
+    .attr('y', d => yPositionScale(d.Cost_1_GB_USD))
+    .attr('fill', 'lightgrey')
+    .attr('class', d => d.Region.toLowerCase().replace(/[^a-z]*/g, ''))
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+
+  d3.select('#asia').on('click', function() {
+    console.log('clicked')
+    svg.selectAll('rect').attr('fill', 'lightgrey')
+    svg.selectAll('.asia').attr('fill', 'lightskyblue')
+  })
+
+  d3.select('#africa').on('click', function() {
+    console.log('clicked')
+    svg.selectAll('rect').attr('fill', 'lightgrey')
+    svg.selectAll('.africa').attr('fill', 'lightskyblue')
+  })
+
+  d3.select('#high-cost').on('click', function() {
+    console.log('clicked')
+    svg
+      .selectAll('rect')
+      .data(datapoints)
+      .attr('fill', function(d) {
+        if (+d.Cost_1_GB_USD > 15) {
+          return 'lightskyblue'
+        } else {
+          return 'lightgrey'
+        }
+      })
+  })
+
+  d3.select('#reset').on('click', function() {
+    svg.selectAll('rect').attr('fill', 'lightgrey')
+  })
+
+  d3.select('#continent').on('click', function() {
+    svg.selectAll('.americas').attr('fill', '#D8BFD8')
+    svg.selectAll('.asia').attr('fill', '#F0E68C')
+    svg.selectAll('.africa').attr('fill', '#8FBC8B')
+    svg.selectAll('.europe').attr('fill', '#F4A460')
+    svg.selectAll('.oceania').attr('fill', '#FA8072')
+  })
+
+  const yAxis = d3
+    .axisLeft(yPositionScale)
+    .tickSize(-width)
+    .ticks(4)
+
+  svg
+    .append('g')
+    .attr('class', 'axis y-axis')
+    .call(yAxis)
+    // .attr('stroke', 'red')
+    .attr('stroke-dasharray', '3 2')
+    .lower()
+
+  d3.select('.y-axis .domain').remove()
+}
